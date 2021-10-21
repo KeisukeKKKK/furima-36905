3fd82699ffe31563b7576f5f35bb7ff42e6686c3
KeisukeKKKK <90385361+KeisukeKKKK@users.noreply.github.com>
2021-10-21 21:19:35 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_193921) do
+ActiveRecord::Schema.define(version: 2021_10_15_003423) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 2021_10_16_193921) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "birthday"
-    t.string "text"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
