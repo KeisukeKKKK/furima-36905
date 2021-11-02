09f27b73287116412f74f2a4e05209234b0f48e6
KeisukeKKKK <90385361+KeisukeKKKK@users.noreply.github.com>
2021-11-02 16:58:02 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  #before_action :move_to_index, only: [:edit, :destroy]
-  #before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index, only: [:edit]
+    #, :destroy]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -50,12 +51,12 @@ end
        :shipment_id, :scheduled_id, :image).merge(user_id: current_user.id)
   end
 
-  #def move_to_index
-    #@item = Item.find(params[:id])
-    #unless user_signed_in? && current_user.id == @item.user_id
-      #redirect_to action: :index
-    #end
-  #end
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
 
   def set_item
     @item = Item.find(params[:id])
